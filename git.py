9ea0518369cf4882838360badcd3da62bdb3f0f3
Sarthak Srivastava <95467726+s-sarthak@users.noreply.github.com>
2021-12-03 23:34:11 +0530

    Add files via upload

---
+++ git.py
@@ -0,0 +1,32 @@
+Step 1:
+
+    Create a repository on Github. Visit github.com.
+
+Step 2:
+
+   Initialize Git
+   git init
+
+Step 3:
+
+   Add files to repository.
+   git add .
+
+Step 3:
+
+   Commit your added files to repository.
+   git commit -m "changes info"
+
+Step 4:
+
+   Copy the repository url
+   git remote add origin  url
+
+Step 5:
+
+   Push your changes to master branch
+   git push origin master or git push -f origin master
+
+git push origin HEAD:main
+
+gigt status
